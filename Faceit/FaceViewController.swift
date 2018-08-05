40765da50d39431d320ab670e8217e0d443deedc
Adam Schussheim <Adam@adams-macbook-air.local>
2018-08-05 12:26:04 -0400

    Initial Commit

---
+++ Faceit/FaceViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Faceit
+//
+//  Created by Adam Schussheim on 7/19/18.
+//  Copyright © 2018 Adam Schussheim. All rights reserved.
+//
+
+import UIKit
+
+class FaceViewController: UIViewController
+{
+    var expression = FacialExpression(eyes: .Open, eyeBrows: .Normal, mouth: .Smile)
+}
