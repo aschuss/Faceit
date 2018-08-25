1a78cae5268b37b43b760d8a37a1c8120235ff5c
Adam Schussheim <Adam@adams-air.home>
2018-08-25 18:16:19 -0400

    starting segues

---
+++ Faceit/EmotionsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  EmotionsViewController.swift
+//  Faceit
+//
+//  Created by Adam Schussheim on 8/19/18.
+//  Copyright © 2018 Adam Schussheim. All rights reserved.
+//
+
+import UIKit
+
+class EmotionsViewController: UIViewController {
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
